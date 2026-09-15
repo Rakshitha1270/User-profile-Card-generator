873298d82dbeb59c5868567b92e298380187a90a
Rakshitha1270 <rakshithakasam@gmail.com>
2026-09-15 14:05:37 +0530

    Add route to serve index.html file

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 const db = new sqlite3.Database("./profiles.db");
 
 db.serialize(() => {
